01ea3f21250ccdb2bacd00178f03dbfd6763a3a8
ioanaydgn <mertaydogn0@gmail.com>
2024-03-17 10:29:29 +0200

    .

--- src/routes/v1/customrole.router.ts
+++ src/routes/v1/customrole.router.ts
@@ -1,34 +1,16 @@
 import customroleController from "controllers/v1/customrole.controller";
-
 import { Router } from "express";
-import { authToken, roleConstraint } from "middlewares";
+
+import { authToken } from "middlewares";
 
 let customRoleRouter = Router();
 
-customRoleRouter.post(
-  "/create",
-  roleConstraint("admin"),
-  customroleController.CreateCustomRole
-);
-customRoleRouter.delete(
-  "/:customRoleId",
-  roleConstraint("admin"),
-  customroleController.DeleteCustomRole
-);
-customRoleRouter.put(
-  "/:customRoleId",
-  roleConstraint("admin"),
-  customroleController.UpdateCustomRole
-);
-customRoleRouter.get(
-  "/:customRoleId",
-  roleConstraint("employee"),
-  customroleController.GetCustomRoleById
-);
-customRoleRouter.get(
-  "/organization/:organizationId",
-  roleConstraint("manager"),
-  customroleController.GetAllCustomRoles
-);
+
+customRoleRouter.post("/create",(req, res) => customroleController.CreateCustomRole(req, res));
+customRoleRouter.delete("/:customRoleId" ,(req, res) => customroleController.DeleteCustomRole(req, res));
+customRoleRouter.put("/:customRoleId" ,(req, res) => customroleController.UpdateCustomRole(req, res));
+customRoleRouter.get("/:customRoleId",(req, res) => customroleController.GetCustomRoleById(req, res));
+customRoleRouter.get("/o/:organizationId",(req, res) => customroleController.GetAllCustomRoles(req, res));
+
 
 export default customRoleRouter;
